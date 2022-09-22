a666433be782fc366a4ce097a5edcf862ea3296f
garogross <88031197+garogross@users.noreply.github.com>
2022-09-22 23:45:33 +0400

    creating all pages

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,9 @@ import * as flsFunctions from "./modules/testWebp.js"
 flsFunctions.isWebp()
 
 
+//? page replacing
+
+window.addEventListener("load", () => {
+   const pageHref = window.location.href 
+   console.log(pageHref);
+})
