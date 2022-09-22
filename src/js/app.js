45eb5a40aa7377688ae9a7b5d177533580a914d6
Levon <levonbakunts3@gmail.com>
2022-09-23 02:37:39 +0400

    Create burger and false fixed to commit and change elements

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,21 @@ flsFunctions.isWebp()
 //? page replacing
 
 window.addEventListener("load", () => {
-   const pageHref = window.location.href 
-   console.log(pageHref);
+  const pageHref = window.location.href
+  console.log(pageHref);
+})
+
+/*************** BURGER MENU **************************/ 
+
+const openMobileMenuBtn = document.querySelector(".device-menu");
+const headerMenu = document.querySelector(".header-nav");
+
+openMobileMenuBtn.addEventListener("click", () => {
+  if (openMobileMenuBtn.classList.contains("open")) {
+    openMobileMenuBtn.classList.remove("open");
+    headerMenu.classList.remove("active");
+  } else {
+    headerMenu.classList.add("active");
+    openMobileMenuBtn.classList.add("open");
+  }
 })
