a030318b9c1b21a8809300a574b600d31d1fe9dc
Levon <levonbakunts3@gmail.com>
2022-09-28 01:17:20 +0400

    Finished

--- src/php/sendMail.php
+++ src/php/sendMail.php
@@ -9,10 +9,10 @@ require  "PHPMailer/src/SMTP.php";
 
 $mail = new PHPMailer(true);
 $mail->isSMTP();
-$mail->Host = 'smtp.gmail.com';
+$mail->Host = 'host6.name.am';
 $mail->SMTPAuth = true;  
-$mail->Username = 'levonbakunts3@gmail.com';
-$mail->Password = 'mgls nccd usnn ggtn'; 
+$mail->Username = 'noreply@siriuslogistics.am';
+$mail->Password = 'Z04CgoaT0GaSOwzp'; 
 $mail->SMTPSecure = 'ssl';
 $mail->Port = 465;
 $mail->CharSet = "UTF-8"; 
@@ -29,23 +29,29 @@ $body = str_replace('%name%', $name, $body);
 $body = str_replace('%email%', $email, $body); 
 $body = str_replace('%message%', $message, $body);
 
-$mail->addAddress("levonbakunts3@gmail.com");
-$mail->setFrom($email,$name);
-$mail->Subject = 'Отправка из формы Sirius Logistics'; 
-$mail->MsgHTML($body);
 
+$mail->addAddress("office@siriuslogistics.am");
+$mail->MsgHTML($body);
+$mail->setFrom($email,$name);
+$mail->Subject = 'Submitting from a form Sirius Logistics';
+
+$mail->send();
+$mail->clearAddresses();
+$mail->setFrom("office@siriuslogistics.am");
+$mail->addAddress($email);
+$mail->msgHTML('<h1>Your letter has been successfully sent!</h1>');
+$mail->Subject = ' Sirius Logistics';
 
 if (!$mail->send()) {
   $message = "Ошибка отправки";
+
 } else {
+
   $message = "Данные отправлены!";
 }
 
-
 $response = ["message" => $message];
-
 
 header('Content-type: application/json');
 echo json_encode($response);
-
 ?>
